a83f3d7bf07603fbd9de872b5eae6980066720fc
Leon Yip <lyip1992@gmail.com>
2015-06-15 20:08:17 -0700

    installed bootstrap and attempting to create data binding between multiple views factory to be implemented

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -1,9 +1,10 @@
 angular.module('notes.home', [])
   .controller('HomeController', function($scope) {
-    $scope.notes = {};
+    $scope.notes = [];
 
     $scope.addNote = function() {
-
+      $scope.notes.push({text: $scope.noteText });
     };
-    
+
+
   });
