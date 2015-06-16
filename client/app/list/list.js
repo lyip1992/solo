64277737a56e4726b30e8632300b19a5d05e4cdc
Leon Yip <lyip1992@gmail.com>
2015-06-16 14:01:46 -0700

    implemented ability to remove items

--- client/app/list/list.js
+++ client/app/list/list.js
@@ -2,5 +2,10 @@ angular.module('notes.list', [])
   .controller('ListController', ['$scope', 'Shared',
     function($scope, Shared) {
       $scope.list = Shared;
+
+      $scope.remove = function(index) {
+        $scope.list.$remove(index);
+      };
+
     }
   ]);
